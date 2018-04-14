b427a6ad2163fceadcc0e91f8e09e7f513154d21
Group 3 <igordyacserg@gmail.com>
2018-04-14 18:53:52 +0300

    init

---
+++ zad0/zad0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+--------------------------+");
+            Console.WriteLine("|Dyachenko Igor Leonidovich|");
+            Console.WriteLine("+--------------------------+");
+            Console.ReadLine();
+        }
+    }
+}
