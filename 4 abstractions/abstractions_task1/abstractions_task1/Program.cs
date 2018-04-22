56d7264fc201e625e1a10506f2ffa555932af172
Group 3 <igordyacserg@gmail.com>
2018-04-22 21:55:13 +0300

    abstractions

---
+++ 4 abstractions/abstractions_task1/abstractions_task1/Program.cs
@@ -0,0 +1,212 @@
+﻿/* Создайте класс AbstractHandler.  
+ * В теле класса создать методы void Open(), void Create(), void Chenge(), void Delete().  
+ * Создать производные классы XMLHandler, TXTHandler, DOCHandler от базового класса AbstractHandler.  
+ * Написать программу, которая будет выполнять определение документа и для каждого формата должны быть методы открытия, создания, редактирования, удаления определенного формата документа. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace abstractions_task1
+{
+    abstract class AbstractHandler
+    {
+        public virtual void Open(string FullPath) { }
+        public virtual void Create(string FullPath) { }
+        public virtual void Change(string FullPath) { }
+        public virtual void Delete(string FullPath) { }
+    }
+
+    class TXTHandler : AbstractHandler
+    {
+        public override void Open(string FullPath)
+        {
+            using (StreamReader sr = File.OpenText(FullPath))
+            {
+                Console.WriteLine("Text from txt file:");
+                string inner = "";
+                while ((inner = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(inner);
+                }
+            }
+        }
+        public override void Create(string FullPath)
+        {
+            using (FileStream fs = File.Create(FullPath))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes("This .txt file created by C# script");
+                fs.Write(info, 0, info.Length);
+                Console.WriteLine("Done!");
+            }
+        }
+        public override void Change(string FullPath)
+        {
+            Console.WriteLine("Type some new text for text file:");
+            using (StreamWriter sw = File.AppendText(FullPath))
+            {
+                string extraText = Console.ReadLine();
+                sw.WriteLine("\n" + extraText);
+                Console.WriteLine("Done!");
+            }
+        }
+        public override void Delete(string FullPath)
+        {
+            File.Delete(FullPath);
+        }
+    }
+
+    class DOCHandler : AbstractHandler
+    {
+        public override void Open(string FullPath)
+        {
+            using (StreamReader sr = File.OpenText(FullPath))
+            {
+                Console.WriteLine("Text from .doc file:");
+                string inner = "";
+                while ((inner = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(inner);
+                }
+            }
+        }
+        public override void Create(string FullPath)
+        {
+            using (FileStream fs = File.Create(FullPath))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes("This .doc file created by C# script");
+                fs.Write(info, 0, info.Length);
+                Console.WriteLine("Done!");
+            }
+        }
+        public override void Change(string FullPath)
+        {
+            Console.WriteLine("Type some new text for .doc file:");
+            using (StreamWriter sw = File.AppendText(FullPath))
+            {
+                string extraText = Console.ReadLine();
+                sw.WriteLine("\n" + extraText);
+                Console.WriteLine("Done!");
+            }
+        }
+        public override void Delete(string FullPath)
+        {
+            File.Delete(FullPath);
+        }
+    }
+
+    class XMLHandler : AbstractHandler
+    {
+        public override void Open(string FullPath)
+        {
+            using (StreamReader sr = File.OpenText(FullPath))
+            {
+                Console.WriteLine("Text from .xml file:");
+                string inner = "";
+                while ((inner = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(inner);
+                }
+            }
+        }
+        public override void Create(string FullPath)
+        {
+            using (FileStream fs = File.Create(FullPath))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes("This .xml file created by C# script");
+                fs.Write(info, 0, info.Length);
+                Console.WriteLine("Done!");
+            }
+        }
+        public override void Change(string FullPath)
+        {
+            Console.WriteLine("Type some new text for .xml file:");
+            using (StreamWriter sw = File.AppendText(FullPath))
+            {
+                string extraText = Console.ReadLine();
+                sw.WriteLine("\n" + extraText);
+                Console.WriteLine("Done!");
+            }
+        }
+        public override void Delete(string FullPath)
+        {
+            File.Delete(FullPath);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FullPath;
+            Console.Write("Enter path to the file (XML, DOC or TXT): ");
+            FullPath = Console.ReadLine();
+            switch(Path.GetExtension(FullPath))
+            {
+                case (".txt"):
+                    TXTHandler TXT = new TXTHandler();
+                    if(!File.Exists(FullPath))
+                    {
+                        TXT.Create(FullPath);
+                    } else
+                    {
+                        TXT.Open(FullPath);
+                    }
+                    TXT.Change(FullPath);
+                    Console.Write("Do you want to delete this file?: ");
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Y)
+                    {
+                        TXT.Delete(FullPath);
+                    }
+                    else break;
+
+                    break;
+                case (".doc"):
+                    DOCHandler DOC = new DOCHandler();
+                    if (!File.Exists(FullPath))
+                    {
+                        DOC.Create(FullPath);
+                    }
+                    else
+                    {
+                        DOC.Open(FullPath);
+                    }
+                    DOC.Change(FullPath);
+                    Console.Write("Do you want to delete this file?: ");
+                    key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Y)
+                    {
+                        DOC.Delete(FullPath);
+                    }
+                    else break;
+                    break;
+                case (".xml"):
+                    XMLHandler XML = new XMLHandler();
+                    if (!File.Exists(FullPath))
+                    {
+                        XML.Create(FullPath);
+                    }
+                    else
+                    {
+                        XML.Open(FullPath);
+                    }
+                    XML.Change(FullPath);
+                    Console.Write("Do you want to delete this file?: ");
+                    key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Y)
+                    {
+                        XML.Delete(FullPath);
+                    }
+                    else break;
+                    break;
+                default:
+                    Console.WriteLine("Incorrect file type (not txt/doc/xml)!");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 4 abstractions/abstractions_task2/Task2/Program.cs
@@ -0,0 +1,164 @@
+﻿/*
+Создайте 2 интерфейса IPlayable и IRecodable. В каждом из
+интерфейсов создайте по 3 метода void Play() / void Pause() / void
+Stop() и void Record() / void Pause() / void Stop() соответственно.
+Создайте производный класс Player от базовых интерфейсов
+IPlayable и IRecodable.
+Написать программу, которая выполняет проигрывание и запись.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    interface IPlayable
+    {
+        void Play();
+        void Pause();
+        void Stop();
+    }
+
+    interface IRecodable
+    {
+        void Record();
+        void Pause();
+        void Stop();
+    }
+
+    public class Player : IPlayable, IRecodable
+    {
+        void IPlayable.Play()
+        {
+            Console.WriteLine("Music plays");
+        }
+
+        void IPlayable.Pause()
+        {
+            Console.WriteLine("Music on pause");
+        }
+
+        void IPlayable.Stop()
+        {
+            Console.WriteLine("Music stopped");
+        }
+
+        void IRecodable.Record()
+        {
+            Console.WriteLine("Recording started");
+        }
+
+        void IRecodable.Pause()
+        {
+            Console.WriteLine("Record on pause");
+        }
+
+        void IRecodable.Stop()
+        {
+            Console.WriteLine("Record stopped");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int flag1 = 1;
+            int flag2, flag3, flag4;
+            Player player = new Player();
+            while (flag1 == 1)
+            {
+                Console.Clear();
+                flag2 = 1;
+                Console.WriteLine("\nPROGRAMM START \nWhat r u want to open ? \n1 - Player \n2 - Recorder \n3 - EXIT");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        flag4 = 1;
+                        while (flag2 == 1)
+                        {
+                            flag3 = 1;
+                            if (flag4 == 1)
+                                Console.WriteLine("\nWhat next? \n1 - Play \n4 - Main menu");
+                            else
+                                Console.WriteLine("\nWhat next? \n1 - Resume play \n4 - Main menu");
+                            switch (Console.ReadLine())
+                            {
+                                case "1":
+                                    ((IPlayable)player).Play();
+                                    while (flag3 == 1)
+                                    {
+                                        Console.WriteLine("\nWhat next? \n2 - Pause \n3 - Stop");
+                                        switch (Console.ReadLine())
+                                        {
+                                            case "2":
+                                                ((IPlayable)player).Pause();
+                                                flag3 = 0;
+                                                flag4 = 0;
+                                                break;
+                                            case "3":
+                                                ((IPlayable)player).Stop();
+                                                flag3 = 0;
+                                                flag4 = 1;
+                                                break;
+                                        }
+                                    }
+                                    break;
+                                case "4":
+                                    flag2--;
+                                    break;
+                            }
+                        }
+                        break;
+                    case "2":
+                        flag4 = 1;
+                        while (flag2 == 1)
+                        {
+                            flag3 = 1;
+                            if (flag4==1)
+                                Console.WriteLine("\nWhat next? \n1 - Record \n4 - Main menu");
+                            else
+                                Console.WriteLine("\nWhat next? \n1 - Resume record \n4 - Main menu");
+                            switch (Console.ReadLine())
+                            {
+                                case "1":
+                                    ((IRecodable)player).Record();
+                                    while (flag3 == 1)
+                                    {
+                                        Console.WriteLine("\nWhat next? \n2 - Pause \n3 - Stop");
+                                        switch (Console.ReadLine())
+                                        {
+                                            case "2":
+                                                ((IRecodable)player).Pause();
+                                                flag3 = 0;
+                                                flag4 = 0;
+                                                break;
+                                            case "3":
+                                                ((IRecodable)player).Stop();
+                                                flag3 = 0;
+                                                flag4 = 1;
+                                                break;
+                                        }
+                                    }
+                                    break;
+                                case "4":
+                                    flag2--;
+                                    break;
+                            }
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("EXIT \n TY 4 our programm using!");
+                        flag1--;
+                        break;
+                    default:
+                        Console.WriteLine("Unknown command | | | Try again");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
