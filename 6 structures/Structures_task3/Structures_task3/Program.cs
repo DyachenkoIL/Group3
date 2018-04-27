a8712c05606e894983de29e3e4595adf7b57f25e
Group 3 <igordyacserg@gmail.com>
2018-04-27 23:32:42 +0300

    structures

---
+++ 6 structures/Structures_task3/Structures_task3/Program.cs
@@ -0,0 +1,71 @@
+﻿/*Задание 3
+Создайте класс MyClass и структуру MyStruct, которые содержат в себе поля public string change.
+В классе Program создайте два метода:
+- static void ClassTaker(MyClass myClass), который присваивает полю change экземпляра myClass
+значение «изменено».
+- static void StruktTaker(MyStruct myStruct), который присваивает полю change экземпляра myStruct
+значение «изменено».
+Продемонстрируйте разницу в использовании классов и структур, создав в методе Main() экземпляры
+структуры и класса. Измените, значения полей экземпляров на «не изменено», а затем вызовите методы
+ClassTaker и StruktTaker. Выведите на экран значения полей экземпляров. Проанализируйте полученные
+результаты.
+Miroshnichenko Denis*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures_task3
+{
+    /*Класс является ссылочным типом. 
+     * Когда вы создаете объект класса и сохраняете его в переменную, эта переменная содержит только ссылку на память объекта. 
+     * Если ссылка на объект сохраняется в новую переменную, эта переменная также ссылается на исходный объект. 
+     * Изменения, внесенные через одну переменную, отражаются и в другой переменной, поскольку обе они ссылаются на одни и те же данные. */
+    class MyClass
+    {
+        public string change;
+        public MyClass() //Классы могу содержать явные конструкторы без параметров, поля можно не инициализировать
+        {
+            change = "не изменено";
+        }
+    }
+    /*Структура (struct) является типом значения. 
+     * При создании структуры переменная, которой присвоена структура, содержит фактические данные этой структуры. 
+     * Если структура присваивается новой переменной, все данные копируются. Таким образом, новая переменная и исходная переменная содержат две отдельные копии одинаковых данных. 
+     * Изменения, внесенные в одну копию, не влияют на другую. */
+    struct MyStruct
+    {
+        public string change;
+        public MyStruct(string s_in) // Структуры не могут содержать явных конструкторов без параметров, все неинициализированные поля должны быть инициализированы
+        {
+            this.change = s_in;
+        }
+        public void StructTaker(MyStruct myStruct)
+        {
+            this.change = "изменено";
+        }
+    }
+    class Program
+    {
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "изменено";
+        } // сработает, так как экземпляр класса хранится в куче, и ссылаясь на клас можно изменять его поля
+        static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.change = "изменено";
+        } // невозможно изменить значения полей уже созданного экземпляра стурктуры, можно заменить всю структуру, либо упаковать структуру и изменить значение поля объекта в куче
+        static void Main(string[] args)
+        {
+            string test = "не изменено";
+            MyClass myClass = new MyClass();
+            MyStruct myStruct = new MyStruct(test);
+            Console.Write(myStruct.change + "\n" + myClass.change);
+            ClassTaker(myClass);
+            StructTaker(myStruct);
+            Console.Write("\n" + myStruct.change + "\n" + myClass.change);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 6 structures/Structures_task4/Structures_task4/Program.cs
@@ -0,0 +1,53 @@
+﻿/*Реализуйте программу, которая будет принимать от пользователя дату его рождения и выводить количество
+дней до его следующего дня рождения.
+Miroshnichenko Denis*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures_task4
+{ 
+    class Program
+    {
+        static int GetBirthDay(int min_value, int max_value, string value_name)
+        {
+            int value = 0;
+            while (value < min_value || value > max_value)
+            {
+                Console.Write("Type " + value_name + " of your birthday here: ");
+                value = Int32.Parse(Console.ReadLine());
+                if (value < 1 || value > max_value)
+                {
+                    Console.Write("Error, '" + value_name + "' must be between " + min_value + " and " + max_value + "!\nTry again: ");
+                    value = Int32.Parse(Console.ReadLine());
+                }
+            }
+            return value;
+        }
+        static void Main(string[] args)
+        {
+            int year = GetBirthDay(1, 9999, "year");
+            int month = GetBirthDay(1, 12, "month");
+            Console.Write("Count Days in curr month: " + DateTime.DaysInMonth(year, month) + "\n");
+            int day = GetBirthDay(1, DateTime.DaysInMonth(year, month), "day");
+            DateTime n_birthday = new DateTime(DateTime.Now.Year, month, day);
+            //DateTime today = new //DateTime(2018, 4, 27);
+            TimeSpan timeSpan = n_birthday.Subtract(DateTime.Now);
+            int dayoffset;
+            if (timeSpan.Days < 0)
+            {
+                int curryeardayscount = DateTime.IsLeapYear(DateTime.Now.Year) ? 366 : 365;
+                dayoffset = curryeardayscount - DateTime.Now.Day;
+            }  else
+            {
+                dayoffset = timeSpan.Days;
+            }
+
+            Console.WriteLine("Days to the next birthday: " + dayoffset);
+
+            Console.ReadKey();
+        }
+    }
+}
