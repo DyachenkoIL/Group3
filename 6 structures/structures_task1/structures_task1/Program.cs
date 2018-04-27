eb6f89bb7e38fd72838f34d6cb22d39998d7859c
Group 3 <igordyacserg@gmail.com>
2018-04-27 23:19:52 +0300

    structures

---
+++ 6 structures/structures_task1/structures_task1/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+Создайте структуру с именем - Notebook.
+Поля структуры: модель, производитель, цена.
+В структуре должен быть реализован конструктор для инициализации полей и метод для вывода содержимого
+полей на экран*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structures_task1
+{
+    struct Notebook
+    {
+        public string Model
+        {
+            set; get;
+        }
+        public string Made
+        {
+            set; get;
+        }
+        public double Price
+        {
+            set; get;
+        }
+
+        public Notebook(string Model, string Made, double Price)
+        {
+            this.Model = Model;
+            this.Made = Made;
+            this.Price = Price;
+        }
+
+        public void printVal()
+        {
+            Console.WriteLine("\n\nProduct: \n  Model: {0} \n  Country of production: {1} \n  Price: {2}", Model, Made, Price);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many products do you want to add?");
+            int kol = Convert.ToInt32(Console.ReadLine());
+            Notebook[] note = new Notebook[kol];
+            for (int i = 0; i < kol; i++)
+            {
+                Console.Write("\nEnter model of {0} product:", i + 1);
+                note[i].Model = Console.ReadLine();
+                Console.Write("Enter country of production of {0} product:", i + 1);
+                note[i].Made = Console.ReadLine();
+                Console.Write("Enter price of {0} product:", i + 1);
+                double nm;
+                while (!double.TryParse(Console.ReadLine(), out nm))
+                    Console.WriteLine("\nEnter numeric value. Try again!");
+                note[i].Price = nm;
+            }
+            Console.Clear();
+            foreach (Notebook n in note)
+                n.printVal();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 6 structures/structures_task6/ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Создайте перечисление, в котором будут содержаться должности сотрудников как имена констант.
+Присвойте каждой константе значение, задающее количество часов, которые должен отработать сотрудник за
+месяц.
+Создайте класс Accauntant с методом bool AskForBonus(Post worker, int hours), отражающее давать
+или нет сотруднику премию. Если сотрудник отработал больше положеных часов в месяц, то ему положена
+премия. 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        enum Post
+        {
+            janitor = 80,
+            porter = 120,
+            chief = 90
+        }
+        
+        static class Accauntant
+        {
+            static public bool AskForBonus(Post worker, int hours)
+            {
+                return ((int)worker<hours);
+            }
+        }
+
+        static void Main(string[] args)
+        {   
+            Post ourchief=Post.chief;
+            if (Accauntant.AskForBonus(ourchief, 80))
+                Console.WriteLine(ourchief + " должен получать премию.");
+            else Console.WriteLine(ourchief + " не должен получать премию.");
+
+            Post ourjanitor = Post.janitor;
+            if (Accauntant.AskForBonus(ourjanitor, 100))
+                Console.WriteLine(ourjanitor + " должен получать премию.");
+            else Console.WriteLine(ourjanitor + " не должен получать премию.");
+
+            Console.ReadKey();
+        }
+    }
+}
