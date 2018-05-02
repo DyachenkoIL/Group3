70fecc3300bd90be79d17d6924f70654fe78968c
Group 3 <igordyacserg@gmail.com>
2018-05-03 00:21:17 +0300

    generics

---
+++ 8 generics/generic_task3/Generic_task3/Program.cs
@@ -0,0 +1,66 @@
+﻿/*Создайте класс MyDictionary<TKey,TValue>. Реализуйте в простейшем приближении возможность
+использования его экземпляра Минимально требуемый интерфейс взаимодействия с экземпляром, должен
+включать метод добавления пар элементов, индексатор для получения значения элемента по указанному
+индексу и свойство только для чтения для получения общего количества пар элементов.
+Miroshnichenko Denis*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_task3
+{
+    class MyDitictionary<Tkey, TValue>
+    {
+        private int counter = 0;
+        private Tkey[] arr_key = null;
+        private TValue[] arr_value = null;
+
+        private int Counter
+        {
+            get { return this.counter; }
+        }
+
+        public void Add(Tkey ikey, TValue ivalue)
+        {
+            this.counter++;
+
+            Array.Resize(ref arr_key, counter);
+            arr_key[counter - 1] = ikey;
+
+            Array.Resize(ref arr_value, counter);
+            arr_value[counter - 1] = ivalue;
+        }
+
+        public TValue this[Tkey key]
+        {
+            get
+            {
+                int ind = 0;
+                for (int i = 0; i < arr_key.Length; i++)
+                {
+                    if (key.Equals(arr_key[i]))
+                    {
+                        ind = i;
+                    }
+                }
+                return arr_value[ind];
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDitictionary<int, string> md = new MyDitictionary<int, string>();
+            md.Add(0, "Hello");
+            md.Add(1, "there,");
+            md.Add(2, "Everyone");
+
+            Console.WriteLine(md[1]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 8 generics/generic_task5/Generic_task5/Program.cs
@@ -0,0 +1,90 @@
+﻿/*Создайте класс CarCollection<T>.Реализуйте в простейшем приближении возможность использования его
+экземпляра для создания парка машин.Минимально требуемый интерфейс взаимодействия с экземпляром,
+должен включать метод добавления машин с названием машины и года ее выпуска, индексатор для получения
+значения элемента по указанному индексу и свойство только для чтения для получения общего количества
+элементов.
+Создайте метод удаления всех машин автопарка.
+Miroshnichenko Denis
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_task5
+{
+    interface Car
+    {
+        string CarName { get; set; }
+        int YearOfCreation { get; set; }
+    }
+
+    class CarColletion<T> where T: Car, new()
+    {
+        List<T> Car_list = new List<T>();
+
+        public void Add(string Name, int Year)
+        {
+            Car_list.Add(new T() { CarName = Name, YearOfCreation = Year});
+        }
+
+        public T this[int num]
+        {
+            get { return Car_list[num]; }
+            set { Car_list[num] = value; }
+        }
+
+        public int Count
+        {
+            get { return Car_list.Count; }
+        }
+
+        public void Clear()
+        {
+            Car_list.Clear();
+        }
+    }
+
+    class CarA : Car
+    {
+        public string CarName { get; set; }
+        public int YearOfCreation { get; set; }
+        public override string ToString()
+        {
+            return $"{CarName} {YearOfCreation}";//"Name: " + CarName + "\nYear: " + YearOfCreation;
+        }
+    }
+
+    class CarB : Car
+    {
+        public string CarName { get; set; }
+        public int YearOfCreation { get; set; }
+        public override string ToString()
+        {
+            return $"{CarName} {YearOfCreation}";//"Name: " + CarName + "\nYear: " + YearOfCreation;
+        }
+    }
+
+    class Program
+    {
+        static void Create<T>() where T: Car, new()
+        {
+            var collection = new CarColletion<T>();
+            collection.Add("BMW", 1988);
+            collection.Add("Mersedes", 2015);
+            Console.WriteLine("Typeof collection: " + typeof(T).Name);
+            for(int i = 0; i < collection.Count; i++)
+            {
+                Console.WriteLine(collection[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Create<CarA>();
+            Create<CarB>();
+            
+            Console.ReadKey();
+        }
+    }
+}
