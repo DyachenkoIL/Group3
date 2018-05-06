a194d3fea81361a660c3895a13a2e628568818de
Group 3 <igordyacserg@gmail.com>
2018-05-06 23:08:26 +0300

    io

---
+++ 10 io/io_task1/io_task1/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Создайте на диске 100 директорий с именами от Folder_0 до Folder_99, затем удалите их.
+Махинько Виталий
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace io_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    string path = @"E:\test\Folder_" + i;
+                    Directory.CreateDirectory(path);
+                }
+                Console.ReadKey();
+                for (int i = 0; i < 100; i++)
+                {
+                    string path = @"E:\test\Folder_" + i;
+                    Directory.Delete(path);
+                }
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
---
+++ 10 io/io_task2/io_task2/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+Создайте файл, запишите в него произвольные данные и закройте файл. Затем снова откройте этот
+файл, прочитайте из него данные и выведете их на консоль. 
+
+Дяченко Игорь
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace io_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User[] trains = new User[3];
+            trains[0] = new User(1963,"Andrew","Kokado");
+            trains[1] = new User(1979,"Kapitalist","Kapitalistoff");
+            trains[2] = new User(2000,"Igor", "Dyachenko");
+            string path = @"D:\special\file.dat";
+            
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    foreach (User train in trains)
+                    {
+                        writer.Write(train.Number + " ");
+                        writer.Write(train.FirstStat + " ");
+                        writer.WriteLine(train.EndStat);
+                    }   
+                }
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+            
+        }
+
+        struct User
+        {
+            int Year;
+            string Name, Name2;
+
+            public int Number { get { return Year; } }
+            public string FirstStat { get { return Name; } }
+            public string EndStat { get { return Name2; } }
+
+            public User(int num,string first,string end)
+            {
+                Year = num;
+                Name = first;
+                Name2 = end;
+            }
+        }
+    }
+}
